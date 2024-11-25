8cf2cd6c3f1e6488a162de73c5161d29658ddc71
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2024-11-25 20:11:30 -0300

    Clicking on clients table to edit the client

--- lab3/atividade2/src/components/Clients.vue
+++ lab3/atividade2/src/components/Clients.vue
@@ -4,7 +4,7 @@
       class="align-centerfill-height mx-auto"
       max-width="900"
     >
-    <v-data-table :items="clients"></v-data-table>
+    <v-data-table :items="clients" @click:row="editClient" />
     </v-responsive>
   </v-container>
 </template>
@@ -16,12 +16,15 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      clientIdx: 0,
+      editedClient: {},
       clients: [
         {
           id: '4636d367-876c-8274-6578-f938466fcd4',
           name: 'John Doe',
         }
-      ]
+      ],
+      editionDialog: false
     };
   },
   mounted()
@@ -36,9 +39,13 @@ export default {
         this.clients = response.data
       } catch (e) {
         console.error(`Error on getting all clients (${e})`)
-      } finally {
-        this.clients.reset()
       }
+    },
+    editClient(event)
+    {
+      this.clientIdx = Array.from(event.target.closest('tr').parentNode.children).indexOf(event.target.closest('tr'))
+      this.editedClient = this.clients[this.clientIdx]
+      this.editionDialog = true;
     }
   }
 }
