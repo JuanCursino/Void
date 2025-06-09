7ab1359f20b8ce32da1b6d339488cceb7ff0376f
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-06-09 06:51:41 -0300

    <feat> Composite pattern src

---
+++ ipp002/Composite/pattern/composite/src/main/java/edu/fatec/ipp002/composite/leaf/Folder.java
@@ -0,0 +1,32 @@
+package edu.fatec.ipp002.composite.leaf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.fatec.ipp002.composite.component.FileSystemItem;
+
+public class Folder implements FileSystemItem
+{
+    private String name;
+    private List<FileSystemItem> children = new ArrayList<>();
+
+    public Folder(String name)
+    {
+        this.name = name;
+    }
+
+    public void add(FileSystemItem item)
+    {
+        children.add(item);
+    }
+
+    // @Override
+    public void display(String indent)
+    {
+        System.out.println(indent + "+ Folder: " + name);
+        for (FileSystemItem item : children)
+        {
+            item.display(indent + "  ");
+        }
+    }
+}
